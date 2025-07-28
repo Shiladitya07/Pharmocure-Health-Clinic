950384bd957cf28259af47146900560760767374
Shiladitya <shiladitya@Shiladityas-MacBook-Air.local>
2025-07-28 13:56:05 +0530

    Updated css for home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,25 @@
 import React from "react"
+import "../assets/styles/home.css"
 function Main_logo() {
  
 
   return (
     <>
        <header>
-         <div><h1>Online Health Clinic</h1></div>
-        <div class="brutalist-container">
-        <input
-            placeholder="TYPE HERE"
+          <div className="logo"><h1>Online Health Clinic</h1></div>
+          <div class="search">
+            <input
+            placeholder="TYPE Here"
             class="brutalist-input smooth-type"
             type="text"
-        />
-        <label class="brutalist-label"></label>
+            />
+          <label class="brutalist-label"></label>
+          </div>
+        <div className="loginbutt">
+            <button>Login</button>
         </div>
-       <button>Login</button>
        </header>
+       <br></br>
        <hr></hr> 
     
     </>
