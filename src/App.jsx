6516b75d98311e92084bcf30ff8b1222c87e60f8
Shiladitya <shiladitya@Shiladityas-MacBook-Air.local>
2025-07-31 13:49:56 +0530

    Specialits

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {Main_logo, Nav,CarouselComponent} from "../src/pages/home"
+import {Main_logo, Nav,CarouselComponent,Specialties} from "../src/pages/home"
 
 
 
@@ -10,6 +10,7 @@ function App() {
       <Main_logo />
       <Nav />
       <CarouselComponent />
+      <Specialties />
     </>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,15 @@ import { Carousel } from "react-responsive-carousel";
 import Avail from "../assets/images/24x7_avail.jpeg"
 import Care from "../assets/images/Care.jpeg"
 import Top from "../assets/images/topdoctors.jpeg"
+import "../assets/styles/specialties.css"
+import Gyno from "../assets/images/gyneaco.png"
+import Neu from "../assets/images/neurology.png"
+import Ortho from "../assets/images/orthopedic.png"
+import Dent from "../assets/images/dentist.png"
+import Derma from "../assets/images/derma.png"
+import Gastro from "../assets/images/gastro.png"
+import Pidea from "../assets/images/pedia.png"
+import Genaral from "../assets/images/consult.png"
 
 
 function Main_logo() {
@@ -80,4 +89,51 @@ const CarouselComponent = () => {
   );
 };
 
-export {Main_logo,Nav,CarouselComponent}
+// specialitis//
+
+const specialties = [
+   { name: "General Physician", icon: Genaral },
+   { name: "Dermatology", icon: Derma },
+  { name: "Obstetrics & Gynaecology", icon: Gyno },
+  { name: "Orthopaedics", icon: Ortho },
+  // { name: "ENT", icon: generalIcon },
+  { name: "Neurology", icon: Neu },
+  // { name: "Cardiology", icon: generalIcon },
+  // { name: "Urology", icon: generalIcon },
+   { name: "Gastroenterology", icon: Gastro },
+  // { name: "Psychiatry", icon: generalIcon },
+     { name: "Paediatrics", icon: Pidea },
+  // { name: "Pulmonology", icon: generalIcon },
+  // { name: "Endocrinology", icon: generalIcon },
+  // { name: "Nephrology", icon: generalIcon },
+  // { name: "Neurosurgery", icon: generalIcon },
+  // { name: "Rheumatology", icon: generalIcon },
+  // { name: "Ophthalmology", icon: generalIcon },
+  // { name: "Surgical Gastroenterology", icon: generalIcon },
+  // { name: "Infectious Disease", icon: generalIcon },
+  // { name: "Laparoscopic Surgery", icon: generalIcon },
+  // { name: "Psychology", icon: generalIcon },
+  // { name: "Medical Oncology", icon: generalIcon },
+  // { name: "Diabetology", icon: generalIcon },
+  { name: "Dentist", icon: Dent }
+];
+
+const Specialties = () => {
+  return (
+    <div className="specialties-section">
+      <h2>Browse by Specialties</h2>
+      <div className="specialties-grid">
+        {specialties.map((item, index) => (
+          <div className="specialty-card" key={index}>
+            <div className="icon-wrapper">
+              <img src={item.icon} alt={item.name} />
+            </div>
+            <p>{item.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export {Main_logo,Nav,CarouselComponent,Specialties}
+
